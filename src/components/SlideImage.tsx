ae5d6f26469383d3670054c2e6e42456a287c785
helloimela <elayudhanira@gmail.com>
2024-01-18 10:23:48 +0100

    feat: css variable

--- src/components/SlideImage.tsx
+++ src/components/SlideImage.tsx
@@ -4,11 +4,11 @@ import cn from 'classnames';
 import styles from './SlideImage.module.scss';
 import { SlideImageProps } from './types';
 
-export const SlideImage: FC<SlideImageProps> = ({ src, offset, isActive, className }) => {
+export const SlideImage: FC<SlideImageProps> = ({ src, isActive, className }) => {
   return (
     <div
       className={cn(styles.slideImage, isActive ? styles.active : '', styles[className])}
-      style={{ backgroundPositionX: `${offset}`, backgroundImage: `linear-gradient(0deg, rgba(6,6,6,0.5) 0%, rgba(65,65,65,0.1) 80%), url(${src})` }}
+      style={{ backgroundImage: `linear-gradient(0deg, rgba(6,6,6,0.5) 0%, rgba(65,65,65,0.1) 80%), url(${src})` }}
     />
   );
 };
--- src/components/SlideItem.tsx
+++ src/components/SlideItem.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames';
 import { SlideImage } from './SlideImage';
 import styles from './SlideItem.module.scss';
 import { SlideItemProps } from './types';
+import * as React from 'react';
 
 export const SlideItem: FC<SlideItemProps> = ({ images, url, title, active, className }) => {
   const [hoverOffset, setHoverOffset] = useState<string>('50%');
@@ -35,14 +36,14 @@ export const SlideItem: FC<SlideItemProps> = ({ images, url, title, active, clas
   return (
     <div className={cn(styles.slideItemContainer, active ? styles.activeSlide : '', styles[className])} onMouseMove={(e) => handleSlideHover(e)}>
         <h2 className={styles.slideTitle}>{title}</h2>
-        <ul className={cn(styles.slideListParent)}>
+        <ul className={cn(styles.slideListParent)} style={{"--offsetX": `${hoverOffset}`} as React.CSSProperties}>
           {images.map(({ src }, index) => (
             <li
               key={index}
               className={cn(styles.slideItemList)}
               style={{transform: `translateY(${offsetY[index]})`}}
             >
-              <SlideImage src={src} offset={hoverOffset} isActive={active} className={className} />
+              <SlideImage src={src} isActive={active} className={className} />
             </li>
           ))}
         </ul>
